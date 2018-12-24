ed9af4ef99eb8d41d5af719420e7ae7b63b4df53
Michal Latoszek <michal.latoszek@radx.pl>
2018-12-24 10:11:01 +0100

    Add project files.

---
+++ kmd-logic-digitalpost-console-sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kmd_logic_digitalpost_console_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
